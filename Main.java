f219f0ff21632c70ff774a14764c173d19f85ef0
Zee <110918398+NoMaoZee@users.noreply.github.com>
2023-10-26 11:51:13 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,54 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleCalculator {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Kalkulator Sederhana");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 400);
+        frame.setLayout(new BorderLayout());
+
+        JTextField display = new JTextField();
+        display.setFont(new Font("Arial", Font.PLAIN, 24));
+        frame.add(display, BorderLayout.NORTH);
+
+        JPanel buttons = new JPanel(new GridLayout(4, 4));
+        frame.add(buttons, BorderLayout.CENTER);
+
+        String[] buttonLabels = {
+            "7", "8", "9", "/",
+            "4", "5", "6", "*",
+            "1", "2", "3", "-",
+            "0", ".", "=", "+"
+        };
+
+        for (String label : buttonLabels) {
+            JButton button = new JButton(label);
+            button.setFont(new Font("Arial", Font.PLAIN, 18));
+            buttons.add(button);
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (label.equals("=")) {
+                        String expression = display.getText();
+                        try {
+                            String result = evaluateExpression(expression);
+                            display.setText(result);
+                        } catch (Exception ex) {
+                            display.setText("Error");
+                        }
+                    } else {
+                        display.setText(display.getText() + label);
+                    }
+                }
+            });
+        }
+
+        frame.setVisible(true);
+    }
+
+    private static String evaluateExpression(String expression) {
+        return String.valueOf(new ScriptEngineManager().getEngineByName("JavaScript").eval(expression));
     }
 }
